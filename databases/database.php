7c56fc7d3bba4577ad4d0943e65dd68195d24758
nextaliakbar <stevenaliakabar04@gmail.com>
2024-12-23 21:36:44 +0800

    Create file .cpanel.yml and modifying configuration database

--- databases/database.php
+++ databases/database.php
@@ -1,8 +1,8 @@
 <?php
 $host = 'localhost';        
-$username = 'root';         
-$password = '';            
-$dbname = 'worksmart1'; 
+$username = 'worksmar_admin';         
+$password = 'UIZaCzx_;2@b';            
+$dbname = 'worksmar_worksmartful_db'; 
 
 $conn = new mysqli($host, $username, $password, $dbname);
 
